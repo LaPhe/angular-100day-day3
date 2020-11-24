44fa577356322d0e0b8d1770505e3a3217b50f53
LaPhe <philipny105@gmail.com>
2020-11-24 09:30:29 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+
+  //DATA BIDING
+  //EVEN BIDING
+  user = {
+    name: "PhilipLpy",
+    age: 21
+  };
+
+  handler(event) {
+    console.log("clicked", event);
+  }
 }
